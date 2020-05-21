c66fdd2f3798aa8f957955d3f4f4d9d982e43df6
di2mot <motsar.dm@gmail.com>
2020-05-22 00:38:59 +0300

    Update
    Сделал в стиле ООП

--- Client/client.py
+++ Client/client.py
@@ -1,10 +1,44 @@
 # Echo client program
 import socket
+import threading
+import sys
+import time
 
-HOST = '127.0.0.1'    # The remote host
-PORT = 49906              # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+
+class Client:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send_message(self):
+        while True:
+
+            try:
+                # print('Send message: ')
+                message = input('Send message: ')
+                self.sock.send(bytes(message, 'utf-8'))
+                # time.sleep(0.1)
+            except EOFError:
+                print('Something gona wrong.')
+                sys.exit(1)
+
+
+
+    def __init__(self):
+        # запускаем сервер, заставляя слушать локалхост
+        self.sock.connect(('localhost', 4242))
+        thread = threading.Thread(target=self.send_message)
+        thread.daemon = True
+        thread.start()
+
+
+        while True:
+            print(threading.active_count())
+            data = self.sock.recv(1024)
+            if not data:
+                break
+            print(str(data, 'utf-8'))
+
+
+
+if __name__ == '__main__':
+    client = Client()
+    client.send_message()
--- Server/server.py
+++ Server/server.py
@@ -1,15 +1,53 @@
 # Echo server program
 import socket
+import threading
 
-HOST = '127.0.0.1'                 # Symbolic name meaning all available interfaces
-PORT = 49906              # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
+
+class Server:
+    """docstring for Server"""
+    # Создаём соккет
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conections = [] # наш пулл подключений, поменять на DB
+
+    def __init__(self):
+
+        # запускаем сервер, заставляя слушать локалхост
+        self.sock.bind(('localhost', 4242))
+        # сервер ждёт
+        self.sock.listen(1)
+        print('[Server is working...]')
+
+    def heandler(self, conn, adr):
         while True:
             data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+            for connection in self.conections:
+                connection.send(data)
+            if not data:
+                # просто рисуем крисивый вывод
+                text = 'Disconnetcted'
+                print(' ' * (len(text) + len(str(adr[0])) // 2), 'ip  ', ' ' * (len(str(adr[1])) // 2), 'port')
+                print(f'[{text}: {adr[0]} : {adr[1]}]')
+                self.conections.remove(conn)
+                conn.close()
+                break
+
+    def run(self):
+        while True:
+            conn, adr = self.sock.accept()
+            cTread = threading.Thread(target=self.heandler, args=(conn, adr))
+            cTread.demon = True
+            cTread.start()
+            self.conections.append(conn)
+            data = self.conections
+
+            # adr это tuple, adr[0] -ip, adr[1] - port
+            # просто рисуем крисивый вывод
+            text = 'Connetcted'
+            print(' ' * (len(text) + len(str(adr[0])) // 2), 'ip  ', ' ' * (len(str(adr[1])) // 2), 'port')
+            print(f'[{text}: {adr[0]} {adr[1]}]')
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.run()
+
