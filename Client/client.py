febc6f15463d5a28935815caadf0c50ad358da00
di2mot <motsar.dm@gmail.com>
2020-05-22 21:50:09 +0300

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -11,27 +11,41 @@ class Client:
     def send_message(self):
         while True:
 
+            if threading.currentThread().getName() == 'MainThread':
+                # если  текущий поток главный, то выходим
+                print('Exit')
+                self.sock.close()   # закрываем соединение
+                sys.exit(1)         # выходим из скрипта
+
             try:
-                # print('Send message: ')
-                message = input('Send message: ')
-                self.sock.send(bytes(message, 'utf-8'))
-                # time.sleep(0.1)
+
+                # print('Send message: ', end='')
+                message = input()
+                if message == '/stop':          # для выхода из программы
+                    self.kill_treads = True     # выключает главный цикл
+                else:
+                    self.sock.send(bytes(message, 'utf-8'))
+
+
             except EOFError:
-                print('Something gona wrong.')
-                sys.exit(1)
+                print('[Something gona wrong. Connection lost]')
+                self.kiil_treads = True
+
 
 
 
     def __init__(self):
+        self.kill_treads = False
         # запускаем сервер, заставляя слушать локалхост
         self.sock.connect(('localhost', 4242))
+        print('Соединение активированно\n')
         thread = threading.Thread(target=self.send_message)
         thread.daemon = True
         thread.start()
 
 
-        while True:
-            print(threading.active_count())
+        while not self.kill_treads:
+
             data = self.sock.recv(1024)
             if not data:
                 break
